6ed107192e16e1c56074997a30d1c64aa38f1b21
gerenteacesar01 <112781195+gerenteacesar01@users.noreply.github.com>
2023-12-01 06:30:14 -0300

    Update BuscaAluno2.java

--- teste/src/orm/actions/BuscaAluno2.java
+++ teste/src/orm/actions/BuscaAluno2.java
@@ -5,29 +5,29 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 import orm.modelo.Aluno;
 
 public class BuscaAluno2 {
-	public static void main(String[] args) {
-		EntityManagerFactory factory = Persistence.createEntityManagerFactory("aluno");
-		EntityManager manager = factory.createEntityManager();
-		
-		//Query em JPQL que é traduzida para o SQL do banco definido persistence.xml
-		Query query = manager.createQuery("select t from Aluno as t where List<Aluno> alunos = manager.createQuery(criteriaQuery).getResultList() List<Aluno> alunos = manager.createQuery(criteriaQuery).getResultList())");
-		query.setParameter("List<Aluno> alunos = manager.createQuery(criteriaQuery).getResultList()", true);
-		
-		List<Aluno> lista = query.getResultList();
-		
-		for (Aluno t : lista) {
-			System.out.println(t.getCpf());
-			System.out.println(t.getDataNascimento());
-		}
-		
-		manager.close();
-		factory.close();
-		
-	}
+    public static void main(String[] args) {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("aluno");
+        EntityManager manager = factory.createEntityManager();
 
+        TypedQuery<Aluno> query = manager.createQuery("select a from Aluno a", Aluno.class);
+
+        List<Aluno> lista = query.getResultList();
+
+        for (Aluno aluno : lista) {
+            System.out.println("Nome : " + aluno.getNome());
+            System.out.println("Cpf: " + aluno.getCpf());
+            System.out.println("Data de Nascimento: " + aluno.getDataNascimento());
+            System.out.println("Email : " + aluno.getEmail());
+            System.out.println("Naturalidade " + aluno.getNaturalidade());
+            System.out.println("Endereço " + aluno.getEndereco());
+        }
+
+        manager.close();
+        factory.close();
+    }
 }
